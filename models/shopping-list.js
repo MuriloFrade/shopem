c2bc28da1944526bf5497d4d58bab6ff2c1f866c
MuriloFrade <murilofrade@gmail.com>
2015-12-21 09:00:31 -0300

    configuring mongoose

--- models/shopping-list.js
+++ models/shopping-list.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var shoppingListSchema = new mongoose.Schema({
   title: String,
   itens: [],
-  _ownerId: Schema.Types.ObjectId,
-  _sharedWith: []
+  _ownerId: {type: Schema.Types.ObjectId, ref: 'User'},
+  _sharedWith: [{type: Schema.Types.ObjectId, ref: 'User'}]
 });
 
 mongoose.model('ShoppingList', shoppingListSchema);
