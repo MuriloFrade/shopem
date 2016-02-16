3b9bf5321c816a5c1fbf40cc12d1aa5cd160fe21
Murilo <murilofrade@gmail.com>
2016-02-16 11:10:02 -0300

    remove layout from app page

--- routes/app.js
+++ routes/app.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('app');
+  res.render('app', { layout: false });
 });
 
 module.exports = router;
